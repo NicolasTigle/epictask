c9b2c22d07b14db7478b0ff414c7c11574e58172
Nicolas Tigle <nicolastig08@gmail.com>
2022-09-12 23:30:43 -0300

    Adicionando projeto

---
+++ epictaskapi/src/main/java/br/com/fiap/epictaskapi/controller/UserController.java
@@ -0,0 +1,89 @@
+package br.com.fiap.epictaskapi.controller;
+
+import java.util.List;
+import java.util.Optional;
+
+import javax.validation.Valid;
+
+import org.springframework.beans.BeanUtils;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.cache.annotation.CacheEvict;
+import org.springframework.cache.annotation.Cacheable;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Sort.Direction;
+import org.springframework.data.web.PageableDefault;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+import br.com.fiap.epictaskapi.model.User;
+import br.com.fiap.epictaskapi.service.UserService;
+
+@RestController
+@RequestMapping("/api/user")
+public class UserController {
+    // o ideal é configurar o cache para trabalhar com Redis
+
+    @Autowired
+    private UserService service;
+
+    @GetMapping
+    @Cacheable("users")
+    public Page<User> index(@PageableDefault(size=5, sort = "name") Pageable paginacao){
+        return service.listAll(paginacao);
+    }
+
+    @PostMapping
+    public ResponseEntity<User> create(@RequestBody @Valid User user){
+        service.save(user);
+        return ResponseEntity.status(HttpStatus.CREATED).body(user);
+    }
+
+    @GetMapping("{id}")
+    public ResponseEntity<User> show(@PathVariable Long id){
+       return ResponseEntity.of(service.get(id));
+    }
+
+    @DeleteMapping("{id}")
+    @CacheEvict(value = "users", allEntries = true)
+    public ResponseEntity<Object> destroy(@PathVariable Long id){
+        Optional<User> optional = service.get(id);
+
+        if(optional.isEmpty())
+            return ResponseEntity.status(HttpStatus.NOT_FOUND).build();
+
+        service.deleteById(id);
+        return ResponseEntity.status(HttpStatus.NO_CONTENT).build();
+    }
+
+    @PutMapping("{id}")
+    public ResponseEntity<User> update(@PathVariable Long id, @RequestBody @Valid User newUser){
+        // carregar a tarefa do banco
+        Optional<User> optional = service.get(id);
+
+        // verificar se existe a tarefa com esse id
+        if(optional.isEmpty())
+            return ResponseEntity.status(HttpStatus.NOT_FOUND).build();
+
+        // atualizar os dados 
+        User user = optional.get();
+        BeanUtils.copyProperties(newUser, user);
+        user.setId(id);
+
+        // salvar a tarefa
+        service.save(user);
+
+        return ResponseEntity.ok(user);
+
+    }
+}
